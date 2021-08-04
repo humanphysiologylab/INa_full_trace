66498a05c416008825d17342e0edfdc7830c9bb7
Nik <8vero0810@gmail.com>
2021-08-04 21:34:56 +0300

    new_model

---
+++ src/model_ctypes/ina_pipette_linked_minf_tau/ina.h
@@ -0,0 +1,13 @@
+#ifndef _INA_H_
+#define _INA_H_
+
+#define S_SIZE 7
+#define C_SIZE 24
+#define A_SIZE 12
+
+
+void initialize_states_default(double *STATES);
+void compute_algebraic(const double time,  double *STATES, double *CONSTANTS,  double *ALGEBRAIC);
+void compute_rates(const double time,  double *STATES,  double *CONSTANTS,  double *ALGEBRAIC, double *RATES);
+
+#endif

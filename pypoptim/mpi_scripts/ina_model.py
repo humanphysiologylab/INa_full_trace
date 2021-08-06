f26b45c8104baa96ff2055744a122324a9d6385c
Nik <8vero0810@gmail.com>
2021-08-06 15:54:41 +0300

    ina_model with no difficulties

--- pypoptim/mpi_scripts/ina_model.py
+++ pypoptim/mpi_scripts/ina_model.py
@@ -3,6 +3,7 @@ import ctypes
 import pandas as pd
 import numpy as np
 
+
 class InaModel:
 
     def __init__(self, filename_so):
@@ -12,31 +13,48 @@ class InaModel:
 
         # void initialize_states_default(double *STATES)
         ctypes_obj.initialize_states_default.argtypes = [
-            np.ctypeslib.ndpointer(dtype=np.float64, ndim=1, flags='C_CONTIGUOUS')
+            np.ctypeslib.ndpointer(
+                dtype=np.float64, ndim=1, flags='C_CONTIGUOUS')
         ]
         ctypes_obj.initialize_states_default.restype = ctypes.c_void_p
         ctypes_obj.compute_rates.argtypes = [
             ctypes.c_double,
-            np.ctypeslib.ndpointer(dtype=np.float64, ndim=1, flags='C_CONTIGUOUS'),
-            np.ctypeslib.ndpointer(dtype=np.float64, ndim=1, flags='C_CONTIGUOUS'),
-            np.ctypeslib.ndpointer(dtype=np.float64, ndim=1, flags='C_CONTIGUOUS'),
-            np.ctypeslib.ndpointer(dtype=np.float64, ndim=1, flags='C_CONTIGUOUS')
+            np.ctypeslib.ndpointer(dtype=np.float64, ndim=1,
+                                   flags='C_CONTIGUOUS'),
+            np.ctypeslib.ndpointer(dtype=np.float64, ndim=1,
+                                   flags='C_CONTIGUOUS'),
+            np.ctypeslib.ndpointer(dtype=np.float64, ndim=1,
+                                   flags='C_CONTIGUOUS'),
+            np.ctypeslib.ndpointer(
+                dtype=np.float64, ndim=1, flags='C_CONTIGUOUS')
         ]
         ctypes_obj.compute_rates.restype = ctypes.c_void_p
         ctypes_obj.compute_algebraic.argtypes = [ctypes.c_double,
-            np.ctypeslib.ndpointer(dtype=np.float64, ndim=1, flags='C_CONTIGUOUS'),
-            np.ctypeslib.ndpointer(dtype=np.float64, ndim=1, flags='C_CONTIGUOUS'),
-            np.ctypeslib.ndpointer(dtype=np.float64, ndim=1, flags='C_CONTIGUOUS')
-        ]
+                                                 np.ctypeslib.ndpointer(
+                                                     dtype=np.float64, ndim=1,
+                                                     flags='C_CONTIGUOUS'),
+                                                 np.ctypeslib.ndpointer(
+                                                     dtype=np.float64, ndim=1,
+                                                     flags='C_CONTIGUOUS'),
+                                                 np.ctypeslib.ndpointer(
+                                                     dtype=np.float64, ndim=1,
+                                                     flags='C_CONTIGUOUS')
+                                                 ]
         ctypes_obj.compute_algebraic.restype = ctypes.c_void_p
         ctypes_obj.run.argtypes = [
-            np.ctypeslib.ndpointer(dtype=np.float64, ndim=1, flags='C_CONTIGUOUS'),
-            np.ctypeslib.ndpointer(dtype=np.float64, ndim=1, flags='C_CONTIGUOUS'),
-            np.ctypeslib.ndpointer(dtype=np.float64, ndim=1, flags='C_CONTIGUOUS'),
-            np.ctypeslib.ndpointer(dtype=np.float64, ndim=1, flags='C_CONTIGUOUS'),
+            np.ctypeslib.ndpointer(
+                dtype=np.float64, ndim=1, flags='C_CONTIGUOUS'),
+            np.ctypeslib.ndpointer(
+                dtype=np.float64, ndim=1, flags='C_CONTIGUOUS'),
+            np.ctypeslib.ndpointer(
+                dtype=np.float64, ndim=1, flags='C_CONTIGUOUS'),
+            np.ctypeslib.ndpointer(
+                dtype=np.float64, ndim=1, flags='C_CONTIGUOUS'),
             ctypes.c_int,
-            np.ctypeslib.ndpointer(dtype=np.float64, ndim=2, flags='C_CONTIGUOUS'),
-            np.ctypeslib.ndpointer(dtype=np.float64, ndim=2, flags='C_CONTIGUOUS')
+            np.ctypeslib.ndpointer(
+                dtype=np.float64, ndim=2, flags='C_CONTIGUOUS'),
+            np.ctypeslib.ndpointer(
+                dtype=np.float64, ndim=2, flags='C_CONTIGUOUS')
         ]
         ctypes_obj.run.restype = ctypes.c_int
 
@@ -47,11 +65,11 @@ class InaModel:
     def status(self):
         return self._status
 
-    def run(self, A, S, C, df_protocol, df_initial_state_protocol, **kwargs):
+    def run(self, A, S, C, df_protocol, df_initial_state_protocol,
+            return_algebraic=False, **kwargs):
 
         t = df_protocol.t.values
         v_all = df_protocol.v.values
-
 
         t0 = df_initial_state_protocol.t.values
         v0 = df_initial_state_protocol.v.values
@@ -63,9 +81,8 @@ class InaModel:
         initial_state_A = np.zeros((initial_state_len, len(A)))
 
         self._run(S.values.copy(), C.values.copy(),
-        # self._run(S.values.copy(), C.copy(),
-                t0, v0, initial_state_len,
-                initial_state_S, initial_state_A)
+                  t0, v0, initial_state_len,
+                  initial_state_S, initial_state_A)
 
         S_output = np.zeros((output_len, len(S)))
         A_output = np.zeros((output_len, len(A)))
@@ -78,11 +95,11 @@ class InaModel:
             t1 = t[start:end] - t[start]
             len_one_step = split_indices[k + 1] - split_indices[k]
             self._status = self._run(initial_state_S[-1].copy(), C.values.copy(),
-            # self._status = self._run(initial_state_S[-1].copy(), C.copy(),
-                           t1, v, len_one_step,
-                           S_output[start:end], A_output[start:end])
-        output = pd.DataFrame(S_output.copy(), columns=S.index)
-        #output2 = pd.DataFrame(A_output.copy(), columns=A.index)
+                                     t1, v, len_one_step,
+                                     S_output[start:end], A_output[start:end])
+        output_S = pd.DataFrame(S_output.copy(), columns=S.index)
+        output_A = pd.DataFrame(A_output.copy(), columns=A.index)
 
-        #print(output)
-        return output #, output2
+        if return_algebraic:
+            return output_S, output_A
+        return output_S
